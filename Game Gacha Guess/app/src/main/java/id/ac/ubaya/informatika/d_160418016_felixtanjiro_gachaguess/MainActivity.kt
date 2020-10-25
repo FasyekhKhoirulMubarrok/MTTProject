90d2ba45afeb0a5d08ad7338cd8d30803c167e87
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-10-26 01:23:01 +0700

    Prototype Game Gacha Guess

---
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.informatika.d_160418016_felixtanjiro_gachaguess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
