01c88ed09dbb8a93d868f595f3dd29a88436baf3
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-10-28 02:57:09 +0700

    uji coba code3

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class PemainActivity : AppCompatActivity() {
     val test = ""
     val test2 = ""
+    val test3 = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemain)
