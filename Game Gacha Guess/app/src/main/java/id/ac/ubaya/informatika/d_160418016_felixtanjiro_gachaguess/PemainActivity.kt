673e8e89ed1259b8cd1365aef19dd8043bf2fe75
ViReNavy <71750098+ViReNavy@users.noreply.github.com>
2020-10-28 12:35:28 +0700

    update Pemenang dan Pemain activity

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
@@ -1,14 +1,41 @@
 package id.ac.ubaya.informatika.d_160418016_felixtanjiro_gachaguess
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_pemain.*
 
 class PemainActivity : AppCompatActivity() {
-    val test = ""
-    val test2 = ""
-    val test3 = ""
+    companion object{
+        val pemain1 = "pemain1"
+        val pemain2 = "pemain2"
+        val tipeSoal ="tipeSoal"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemain)
+
+        val adapter = ArrayAdapter(this, R.layout.my_spinner_layout, Global.tipeKategoriSoal)
+        adapter.setDropDownViewResource(R.layout.my_spinner_item_layout)
+        spinnerKategori.adapter = adapter
+
+        buttonGachaGuess.setOnClickListener {
+            if(editTextTextPersonName1.text.isNotEmpty() && editTextTextPersonName2.text.isNotEmpty())
+            {
+                val pemain = Intent(this, MainActivity::class.java)
+                pemain.putExtra(pemain1, editTextTextPersonName1.text.toString())
+                pemain.putExtra(pemain2, editTextTextPersonName2.text.toString())
+                pemain.putExtra(tipeSoal, spinnerKategori.selectedItem.toString())
+                startActivity(pemain)
+                finish()
+            }
+            else
+            {
+                Toast.makeText(this, "Masukkan Nama Pemain Terlebih Dahulu", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemenangActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemenangActivity.kt
@@ -1,11 +1,22 @@
 package id.ac.ubaya.informatika.d_160418016_felixtanjiro_gachaguess
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pemenang.*
 
 class PemenangActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemenang)
+
+        textViewPemenang.text = intent.getStringExtra(MainActivity.nama).toString()
+        textViewScorePemenang.text = intent.getStringExtra(MainActivity.skor).toString()
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent(this, PemainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
