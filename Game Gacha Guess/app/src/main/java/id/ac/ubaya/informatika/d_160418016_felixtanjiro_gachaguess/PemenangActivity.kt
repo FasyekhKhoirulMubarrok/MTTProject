256e507eac938ebcdab2bead12846ab84039c014
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-10-29 18:14:48 +0700

    revisi code pemenang

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemenangActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemenangActivity.kt
@@ -9,14 +9,5 @@ class PemenangActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemenang)
-
-        textViewPemenang.text = intent.getStringExtra(MainActivity.nama).toString()
-        textViewScorePemenang.text = intent.getStringExtra(MainActivity.skor).toString()
-    }
-
-    override fun onBackPressed() {
-        val intent = Intent(this, PemainActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
