83924a0cc855f6022dcd7c7162a52fb31e5c9fa8
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-10-29 22:05:34 +0700

    import code

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/MainActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/MainActivity.kt
@@ -1,7 +1,14 @@
 package id.ac.ubaya.informatika.d_160418016_felixtanjiro_gachaguess
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
