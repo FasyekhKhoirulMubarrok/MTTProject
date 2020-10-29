9443455a56ccf5912bf770593521489b7b03d115
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-10-29 18:15:48 +0700

    revisi code pemain 2

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
@@ -17,25 +17,5 @@ class PemainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemain)
-
-        val adapter = ArrayAdapter(this, R.layout.my_spinner_layout, Global.tipeKategoriSoal)
-        adapter.setDropDownViewResource(R.layout.my_spinner_item_layout)
-        spinnerKategori.adapter = adapter
-
-        buttonGachaGuess.setOnClickListener {
-            if(editTextTextPersonName1.text.isNotEmpty() && editTextTextPersonName2.text.isNotEmpty())
-            {
-                val pemain = Intent(this, MainActivity::class.java)
-                pemain.putExtra(pemain1, editTextTextPersonName1.text.toString())
-                pemain.putExtra(pemain2, editTextTextPersonName2.text.toString())
-                pemain.putExtra(tipeSoal, spinnerKategori.selectedItem.toString())
-                startActivity(pemain)
-                finish()
-            }
-            else
-            {
-                Toast.makeText(this, "Masukkan Nama Pemain Terlebih Dahulu", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
