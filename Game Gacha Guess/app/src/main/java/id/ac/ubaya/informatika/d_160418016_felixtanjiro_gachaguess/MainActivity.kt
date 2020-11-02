f9f0b099affa2cc7f6ca39268ad2dc5db96babbf
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-11-02 14:49:10 +0700

    update on create main activity

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/MainActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/MainActivity.kt
@@ -23,14 +23,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         pemain1 = Pemain(intent.getStringExtra(PemainActivity.pemain1).toString(), skorPemain1)
         pemain2 = Pemain(intent.getStringExtra(PemainActivity.pemain2).toString(),skorPemain2)
         tipeSoal = intent.getStringExtra(PemainActivity.tipeSoal).toString()
         textViewKategori.text = tipeSoal
+
         pemain = pemain1
         gantiGiliran(pemain1)
+
         val pertanyaan = kumpulanSoal(tipeSoal)
         random = Random.nextInt(0,pertanyaan.count())
         textViewHint.text = pertanyaan[random].soal
+
+        val totalKarakter = pertanyaan[random].jawaban.indices
+        for(i in totalKarakter)
+        {
+            garisBantuJawaban += "_ "
+        }
+        textViewJawaban.text = garisBantuJawaban
     }
 }
