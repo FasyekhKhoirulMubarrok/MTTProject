0cda25ad4ba26207d57232098fd6ef352f2915ae
ViReNavy <71750098+ViReNavy@users.noreply.github.com>
2020-11-10 13:14:45 +0700

    add voley add internet permission add volley addPlayer

--- Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
+++ Game Gacha Guess/app/src/main/java/id/ac/ubaya/informatika/d_160418016_felixtanjiro_gachaguess/PemainActivity.kt
@@ -3,8 +3,11 @@ package id.ac.ubaya.informatika.d_160418016_felixtanjiro_gachaguess
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_pemain.*
 
 class PemainActivity : AppCompatActivity() {
@@ -31,6 +34,26 @@ class PemainActivity : AppCompatActivity() {
                 pemain.putExtra(tipeSoal, spinnerKategori.selectedItem.toString())
                 startActivity(pemain)
                 finish()
+
+                var q = Volley.newRequestQueue(this)
+                val url = "http://10.0.2.2/mtt/addPlayer.php"
+                var stringRequest = object: StringRequest(com.android.volley.Request.Method.POST, url,
+                    {
+                        Log.d("insert",it)
+                    },
+                    {
+                        Log.d("insert",it.message.toString())
+                    }
+                ){
+                    override  fun  getParams(): MutableMap<String,String>{
+                        var params = HashMap<String,String>()
+                        params.put("namapemain1",editTextTextPersonName1.text.toString())
+                        params.put("namapemain2",editTextTextPersonName2.text.toString())
+                        params.put("kategori", spinnerKategori.selectedItem.toString())
+                        return  params
+                    }
+                }
+                q.add(stringRequest)
             }
             else
             {
